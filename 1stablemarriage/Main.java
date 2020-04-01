0c81464cb00fa424b7546ffa6da2c2c7d1942ccb
Tilde Host <tildeannahost@gmail.com>
2020-04-01 10:06:42 +0200

    fortfarande risky business men testerna körs som de ska

---
+++ 1stablemarriage/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+       // System.out.println("main");
+
+        Solution solution = new Solution();
+       // System.out.println("main2");
+
+        solution.run();
+    }
+}
